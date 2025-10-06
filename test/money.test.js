74f51bdf7b9444ef93daee8a1b5f4afa6d3453c2
cash <cashwugeek@icloud.com>
2025-10-06 16:52:29 +0800

    [ch02] modify test, times return dollar

--- test/money.test.js
+++ test/money.test.js
@@ -5,8 +5,11 @@ describe('money', () => {
     it('test multiplication', () => {
 
         let dollar = new Dollar(5);
-        dollar.times(2);
-        expect(dollar.amount).toEqual(10);
+        var product = dollar.times(2);
+        expect(product.amount).toEqual(10);
+
+        product = dollar.times(3);
+        expect(product.amount).toEqual(15);
     })
 
 });
