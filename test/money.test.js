4d60c81d0a06cc8dc69613ca4fe95510c09ab869
cash <cashwugeek@icloud.com>
2025-10-06 20:40:00 +0800

    [ch04] refactor test multiplication

--- test/money.test.js
+++ test/money.test.js
@@ -5,11 +5,8 @@ describe('money', () => {
     it('test multiplication', () => {
 
         let dollar = new Dollar(5);
-        var product = dollar.times(2);
-        expect(product.amount).toEqual(10);
-
-        product = dollar.times(3);
-        expect(product.amount).toEqual(15);
+        expect(dollar.times(2)).toEqual(new Dollar(10));
+        expect(dollar.times(3)).toEqual(new Dollar(15));
     })
 
 
