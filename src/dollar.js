c28ead66fac0206a085be1bff3e15d531741b628
cash <cashwugeek@icloud.com>
2025-10-06 20:47:14 +0800

    [ch04] make amount field private

--- src/dollar.js
+++ src/dollar.js
@@ -1,17 +1,17 @@
 export class Dollar {
-    amount
+    #amount;
 
     constructor(amount) {
-        this.amount = amount
+        this.#amount = amount
     }
 
     times(multiplier) {
-        return new Dollar(this.amount * multiplier)
+        return new Dollar(this.#amount * multiplier)
     }
 
     equals(another) {
         if (another instanceof Dollar) {
-            return this.amount === another.amount;
+            return this.#amount === another.#amount;
         }
         return false;
     }
