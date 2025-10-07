562ef509420ed15d884240621ee70f0d748c36aa
cash <cashwugeek@icloud.com>
2025-10-07 22:46:00 +0800

    [ch09] add currency fun

--- src/dollar.js
+++ src/dollar.js
@@ -2,12 +2,19 @@ import {Money} from "./money";
 
 export class Dollar extends Money {
 
+    currencyStr;
+
     constructor(amount) {
         super(amount);
+        this.currencyStr = "USD";
     }
 
     times(multiplier) {
         return new Dollar(this._amount * multiplier)
+    }
+
+    currency() {
+        return this.currencyStr;
     }
 }
 
--- src/franc.js
+++ src/franc.js
@@ -1,18 +1,24 @@
 import {Money} from "./money";
-import {Dollar} from "./dollar";
 
 export class Franc extends Money {
+    currencyStr;
 
     constructor(amount) {
         super(amount);
+        this.currencyStr = "CHF";
     }
 
     times(multiplier) {
         return new Franc(this._amount * multiplier)
     }
+
+
+    currency() {
+        return this.currencyStr;
+    }
 }
 
-Money.franc = function(amount) {
+Money.franc = function (amount) {
     return new Franc(amount);
 }
 
