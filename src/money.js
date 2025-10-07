b0ed0c4dc699d0937994b20c49e42b7b445a9914
cash <cashwugeek@icloud.com>
2025-10-07 22:16:23 +0800

    [ch07] money equal add type compare (constructor name)

--- src/money.js
+++ src/money.js
@@ -1,13 +1,15 @@
 export class Money {
 
     _amount;
+
     constructor(amount) {
-       this._amount = amount;
+        this._amount = amount;
     }
 
     equals(another) {
         if (another instanceof Money) {
-            return this._amount === another._amount;
+            return this._amount === another._amount
+                && this.constructor.name === another.constructor.name;
         }
         return false;
     }
