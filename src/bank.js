70f91a6d4f6a26e368b546b044fe269438bc5f17
Cash <cashwugeek@icloud.com>
2025-10-08 17:10:11 +0800

    [ch13] move bank reduce to sum

--- src/bank.js
+++ src/bank.js
@@ -1,11 +1,10 @@
-import {Money} from "./money";
 import {Sum} from "./sum";
 
 export class Bank {
     reduce(source, to) {
         if (source instanceof Sum) {
-            let amount = source.augend._amount + source.addend._amount;
-            return Money.dollar(amount, to);
+            return source.reduce(source, to);
         }
     }
+
 }
--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,5 @@
+import {Money} from "./money";
+
 export class Sum {
 
     augend;
@@ -6,5 +8,10 @@ export class Sum {
     constructor(augend, addend) {
         this.augend = augend;
         this.addend = addend;
+    }
+
+    reduce(to) {
+        let amount = this.augend._amount + this.addend._amount;
+        return Money.dollar(amount, to);
     }
 }
