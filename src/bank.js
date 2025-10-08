7c2f39185edda00c81da3ef01cd77a9c71b9efe9
Cash <cashwugeek@icloud.com>
2025-10-08 17:14:35 +0800

    [ch13] use polymorphism remove not useless code

--- src/bank.js
+++ src/bank.js
@@ -1,16 +1,6 @@
-import {Sum} from "./sum";
-import {Money} from "./money";
-
 export class Bank {
     reduce(source, to) {
-
-        if (source instanceof Money) {
-            return source.reduce(to);
-        }
-
-        if (source instanceof Sum) {
-            return source.reduce(source, to);
-        }
+        return source.reduce(to);
     }
 
 }
