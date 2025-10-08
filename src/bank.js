debee6584424839e8eb2314450cd47d09481b15e
cash <cashwugeek@icloud.com>
2025-10-08 20:57:41 +0800

    [ch14] create bank rate func and hard code

--- src/bank.js
+++ src/bank.js
@@ -1,6 +1,13 @@
 export class Bank {
     reduce(source, to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
+    rate(from, to) {
+        return from === "CHF" && to === "USD" ? 2 : 1;
+    }
+
+    addRate(source, to, rate) {
+
+    }
 }
